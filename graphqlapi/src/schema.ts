eb36d48b192d4f648f4d2cae5305e5edc6eaf6c1
Jack Risse <j.risse14@gmail.com>
2020-07-21 15:46:29 -0500

    crud for user

--- graphqlapi/src/schema.ts
+++ graphqlapi/src/schema.ts
@@ -1,6 +1,7 @@
 import { nexusPrismaPlugin } from 'nexus-prisma'
 import { makeSchema, objectType } from '@nexus/schema'
 
+// TODO: figure out datetime
 const User = objectType({
   name: 'User',
   definition(t) {
@@ -12,15 +13,25 @@ const User = objectType({
   },
 })
 
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.crud.user({ alias: 'getUser' })
+    t.crud.users({ alias: 'getUsers' })
+  },
+})
+
 const Mutation = objectType({
   name: 'Mutation',
   definition(t) {
     t.crud.createOneUser({ alias: 'signupUser' })
+    t.crud.deleteOneUser({ alias: 'deleteUser' })
+    t.crud.updateOneUser({ alias: 'updateUser' })
   },
 })
 
 export const schema = makeSchema({
-  types: [Mutation, User],
+  types: [Query, Mutation, User],
   plugins: [nexusPrismaPlugin()],
   outputs: {
     schema: __dirname + '/../schema.graphql',
